af6b2d317b00a05f4ff81fd616a6360356dbe4f5
Esvaber <59067382+Esvaber@users.noreply.github.com>
2024-03-31 10:37:24 -0300

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -6,5 +6,7 @@ sudo systemctl enable docker
 sudo usermod -aG docker ec2-user
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo yum install amazon-efs-utils -y
 sudo mkdir /home/ec2-user/efs
 sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-0ea4d6c19b88f7706.efs.us-east-1.amazonaws.com:/ /home/ec2-user/efs
+sudo sh -c 'echo "fs-0ea4d6c19b88f7706.efs.us-east-1.amazonaws.com:/ /home/ec2-user/efs nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport,_netdev 0 0" >> /etc/fstab'
